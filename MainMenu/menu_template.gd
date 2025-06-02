0792aec5112f6c035d0bf879f3cc331952b75a52
unknown <isaquediasaraujo2.0@gmail.com>
2025-06-02 00:20:13 -0300

    Animacoes padroes feitas / base solida

--- MainMenu/menu_template.gd
+++ MainMenu/menu_template.gd
@@ -1,20 +1,29 @@
 extends Control
 @export var entra_sai_mouse : AnimationPlayer
-@export var animador_de_opcoes_menu : AnimationPlayer
+@export var animador_de_transicao : AnimationPlayer
 @export var pode_interagir : bool #true
 
-func _ready() -> void:
-	animador_de_opcoes_menu.play("opcoeschegando") 
-	
+var indo_pro_jogo : bool = false #entra no jogo
+var indo_pro_config : bool = false #entra na configuracao
+var indo_pro_caralho : bool = false #sai do jogo
 
+func _ready() -> void:
+	animador_de_transicao.play("entrada") 
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_down"):
+		animador_de_transicao.play("entrada") 
 
 ##CLICKS
 func _on_button_pressed() -> void:
-	print("wow")
+	animador_de_transicao.play("sainda")
+	indo_pro_jogo = true
 func _on_options_pressed() -> void:
-	pass
+	animador_de_transicao.play("sainda")
+	indo_pro_config = true
 func _on_quit_game_pressed() -> void:
-	pass
+	animador_de_transicao.play("sainda")
+	indo_pro_caralho = true
 
 
 ##ENTRADA DE MOUSE
@@ -25,10 +34,28 @@ func _on_options_mouse_entered() -> void:
 func _on_quit_game_mouse_entered() -> void:
 	entra_sai_mouse.play("entrou")
 
+
 ##SAINDA DE MOUSE
 func _on_quit_game_mouse_exited() -> void:
 	entra_sai_mouse.play("saindo")
+	pass
 func _on_options_mouse_exited() -> void:
 	entra_sai_mouse.play("saindo")
+	pass
 func _on_start_game_mouse_exited() -> void:
 	entra_sai_mouse.play("saindo")
+	pass
+
+
+func _on_selesao_de_opcao_animation_finished(_anim_name: StringName) -> void:
+	if _anim_name == "sainda":
+		if indo_pro_jogo:
+			print("para o jogo")
+			return
+		if indo_pro_config:
+			print("para para ali")
+			#get_tree().change_scene_to_file("res://MainMenu/settings_menu.tscn")
+			return
+		if indo_pro_caralho:
+			get_tree().quit()
+			return
