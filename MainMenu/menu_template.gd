3ac4b3cf1a05ac6e4c7829f50a663f607147a8c0
unknown <isaquediasaraujo2.0@gmail.com>
2025-06-02 02:48:19 -0300

    Animacoes feitas Base te menu solida!

--- MainMenu/menu_template.gd
+++ MainMenu/menu_template.gd
@@ -2,28 +2,44 @@ extends Control
 @export var entra_sai_mouse : AnimationPlayer
 @export var animador_de_transicao : AnimationPlayer
 @export var pode_interagir : bool #true
+@export var bt1 : Button
+@export var bt2 : Button
+@export var bt3 : Button
+@export var timer : Timer
 
-var indo_pro_jogo : bool = false #entra no jogo
-var indo_pro_config : bool = false #entra na configuracao
-var indo_pro_caralho : bool = false #sai do jogo
+var goto1 : bool = false #entra no jogo
+var goto2 : bool = false #entra na configuracao
+var goto3 : bool = false #sai do jogo
 
 func _ready() -> void:
 	animador_de_transicao.play("entrada") 
-
+	bt1.show()
+	bt2.show()
+	bt3.show()
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_down"):
 		animador_de_transicao.play("entrada") 
-
+		goto1 = false
+		goto2 = false
+		goto3 = false
+		bt1.show()
+		bt2.show()
+		bt3.show()
+		
+		
 ##CLICKS
 func _on_button_pressed() -> void:
 	animador_de_transicao.play("sainda")
-	indo_pro_jogo = true
+	goto1 = true
+	timer.start()
 func _on_options_pressed() -> void:
 	animador_de_transicao.play("sainda")
-	indo_pro_config = true
+	goto2 = true
+	timer.start()
 func _on_quit_game_pressed() -> void:
 	animador_de_transicao.play("sainda")
-	indo_pro_caralho = true
+	goto3 = true
+	timer.start()
 
 
 ##ENTRADA DE MOUSE
@@ -49,13 +65,28 @@ func _on_start_game_mouse_exited() -> void:
 
 func _on_selesao_de_opcao_animation_finished(_anim_name: StringName) -> void:
 	if _anim_name == "sainda":
-		if indo_pro_jogo:
+		if goto1:
 			print("para o jogo")
+			
 			return
-		if indo_pro_config:
+		if goto2:
 			print("para para ali")
 			#get_tree().change_scene_to_file("res://MainMenu/settings_menu.tscn")
 			return
-		if indo_pro_caralho:
+		if goto3:
 			get_tree().quit()
 			return
+
+
+func _on_timer_timeout() -> void:
+	if goto1:
+		bt2.hide()
+		bt3.hide()
+
+	if goto2:
+		bt1.hide()
+		bt3.hide()
+
+	if goto3:
+		bt2.hide()
+		bt1.hide()
