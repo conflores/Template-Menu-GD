3bae35270b2e8cf76a039e83095903a19ebae843
unknown <isaquediasaraujo2.0@gmail.com>
2025-06-01 21:06:33 -0300

    Botoes com animacoes

--- MainMenu/menu_template.gd
+++ MainMenu/menu_template.gd
@@ -1,20 +1,34 @@
 extends Control
-@export var animador : AnimationPlayer
-func _ready() -> void:
-	pass
-	#anima.play("nome_chegada")
+@export var entra_sai_mouse : AnimationPlayer
+@export var animador_de_opcoes_menu : AnimationPlayer
+@export var pode_interagir : bool #true
 
+func _ready() -> void:
+	animador_de_opcoes_menu.play("opcoeschegando") 
+	
+
+
+##CLICKS
 func _on_button_pressed() -> void:
 	print("wow")
-
 func _on_options_pressed() -> void:
-	
-	pass 
-
+	pass
 func _on_quit_game_pressed() -> void:
-	pass 
+	pass
 
 
+##ENTRADA DE MOUSE
 func _on_start_game_mouse_entered() -> void:
-	animador.play("entrou")
-	pass # Replace with function body.
+	entra_sai_mouse.play("entrou")
+func _on_options_mouse_entered() -> void:
+	entra_sai_mouse.play("entrou")
+func _on_quit_game_mouse_entered() -> void:
+	entra_sai_mouse.play("entrou")
+
+##SAINDA DE MOUSE
+func _on_quit_game_mouse_exited() -> void:
+	entra_sai_mouse.play("saindo")
+func _on_options_mouse_exited() -> void:
+	entra_sai_mouse.play("saindo")
+func _on_start_game_mouse_exited() -> void:
+	entra_sai_mouse.play("saindo")
